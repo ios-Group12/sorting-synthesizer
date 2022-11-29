688989196d0d963346c2c34a153cbf331ba8f4a5
EmrysJT <97559945+EmrysJT@users.noreply.github.com>
2022-11-28 21:10:07 -0500

    edited playback feature

--- SortingSynth/RecordingsViewController.swift
+++ SortingSynth/RecordingsViewController.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 import Parse
+import AVFoundation
+import AVKit
 
-class RecordingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+public var AudioPlayer = AVPlayer()
+public var SelectedRecording = Int()
+
+class RecordingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, AVAudioPlayerDelegate{
     
     @IBOutlet weak var tableView: UITableView!
     
@@ -25,9 +30,12 @@ class RecordingsViewController: UIViewController, UITableViewDataSource, UITable
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        let userQuery = PFQuery(className: "Recording")
+        userQuery.includeKey("author")
+        
         let query = PFQuery(className: "Recording")
         query.includeKeys(["createdAt","name"])
-        query.limit = 20
+        query.limit = 10
         
         query.findObjectsInBackground{(recordings, error) in
             if recordings != nil {
@@ -49,10 +57,6 @@ class RecordingsViewController: UIViewController, UITableViewDataSource, UITable
         // MARK: - Table view data source
     }
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
@@ -61,3 +65,26 @@ class RecordingsViewController: UIViewController, UITableViewDataSource, UITable
 
 
 }
+    func grabRecording(){
+        let soundQuery = PFQuery(className: "Recording")
+        soundQuery.getObjectInBackground(withId:recordings[SelectedRecording!] ,block: { (object : PFObject?, error : Error?) ->  Void in
+            if let AudioFileURLTemp : PFFile = object?.value(forKey: "sound") as? PFFile {
+                print(AudioFileURLTemp)
+                
+                
+                
+                AudioPlayer = AVPlayer(url: NSURL(string: AudioFileURLTemp.url!) as! URL)
+                AudioPlayer.play()
+            }
+            
+            func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+                
+                SelectedRecording = indexPath.row
+                grabRecording()
+            }
+            
+            
+            //        let soundFile = recording["sound"] as! PFFileObject
+            //        let urlString = soundFile.url!
+            //        let url = URL(string: urlString)!
+    
