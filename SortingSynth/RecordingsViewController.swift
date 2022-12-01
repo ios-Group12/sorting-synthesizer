6cdab36656b47ffb8ffabc98d66e9a8d8464bd42
McKerlton Hyppolite <63423854+mchype99@users.noreply.github.com>
2022-12-01 15:55:10 -0500

    Update

--- SortingSynth/RecordingsViewController.swift
+++ SortingSynth/RecordingsViewController.swift
@@ -17,72 +17,83 @@ class RecordingsViewController: UIViewController, UITableViewDataSource, UITable
     
     var recordings = [PFObject]()
     var iDArray = [String]()
-    var buttonNumber = Int()
-    
     @IBOutlet weak var tableView: UITableView!
     
     @IBAction func share(_ sender: UIButton) {
-        
         let soundQuery = PFQuery(className: "Recording")
         soundQuery.getObjectInBackground(withId:iDArray[sender.tag], block: { (object : PFObject?, error : Error?) ->  Void in
             if let AudioFile : PFFileObject = object?.object(forKey: "sound") as? PFFileObject {
                 
-                let AudioFileURLTemp = AudioFile.url!
-                print("URL" + AudioFileURLTemp)
-                let url = NSURL(string: AudioFileURLTemp)
-                //let AudioDataFromURL = NSData(contentsOf: url! as URL)
-                let documentsUrl:URL =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-                let destinationFileUrl = documentsUrl.appendingPathComponent("recording.caf")
+                let audioFileURL = AudioFile.url!
+                let url = URL(string: audioFileURL)
+                let documentsUrl: URL =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                let tempFileUrl = documentsUrl.appendingPathComponent("recording.caf")
                 
                 let sessionConfig = URLSessionConfiguration.default
                 let session = URLSession(configuration: sessionConfig)
-                 
-                let request = URLRequest(url:url! as URL)
+                let request = URLRequest(url: url!)
                 
+                // Delete temporary file if for some reason it wasn't removed before
                 do {
-                    try FileManager.default.removeItem(at: destinationFileUrl)
-                    print("Image has been deleted")
+                    try FileManager.default.removeItem(at: tempFileUrl)
+                    print("Temporary file has been deleted")
                 } catch {
                     print(error)
                 }
-                    
+                
                 let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
-                        if let tempLocalUrl = tempLocalUrl, error == nil {
-                            // Success
-                            if let statusCode = (response as? HTTPURLResponse)?.statusCode {
-                                print("Successfully downloaded. Status code: \(statusCode)")
-                            }
-
-                            
-                            do {
-                                try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
-                            } catch (let writeError) {
-                                print("Error creating a file \(destinationFileUrl) : \(writeError)")
-                            }
-                            
-                        } else {
-                            print("Error took place while downloading a file. Error description: %@", error?.localizedDescription as Any);
+                    if let tempLocalUrl = tempLocalUrl, error == nil {
+                        // Success
+                        if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                            print("Successfully downloaded. Status code: \(statusCode)")
                         }
+                        // Download file
+                        do {
+                            try FileManager.default.copyItem(at: tempLocalUrl, to: tempFileUrl)
+                        } catch (let writeError) {
+                            print("Error creating a file \(tempFileUrl) : \(writeError)")
+                        }
+                    } else {
+                        print("Error took place while downloading a file. Error description: %@", error?.localizedDescription as Any);
                     }
+                }
+                
                 task.resume()
-                let objectsToShare = [destinationFileUrl]
+                // Display share sheet
+                let objectsToShare = [tempFileUrl]
                 let activityController = UIActivityViewController(activityItems: objectsToShare as [Any], applicationActivities: nil)
                 let excludedActivities = [UIActivity.ActivityType.postToFlickr, UIActivity.ActivityType.postToWeibo, UIActivity.ActivityType.print, UIActivity.ActivityType.copyToPasteboard, UIActivity.ActivityType.assignToContact, UIActivity.ActivityType.saveToCameraRoll, UIActivity.ActivityType.addToReadingList, UIActivity.ActivityType.postToFlickr, UIActivity.ActivityType.postToVimeo, UIActivity.ActivityType.postToTencentWeibo]
                 
                 activityController.excludedActivityTypes = excludedActivities
                 
                 self.present(activityController, animated: true, completion: nil)
-//                do {
-//                    try FileManager.default.removeItem(at: destinationFileUrl)
-//                    print("Image has been deleted")
-//                } catch {
-//                    print(error)
-//                }
+                // Action performed on Share Sheet dismissal
+                activityController.completionWithItemsHandler = { (activityType: UIActivity.ActivityType?, completed:
+                Bool, arrayReturnedItems: [Any]?, error: Error?) in
+                    if completed {
+                        do {
+                            try FileManager.default.removeItem(at: tempFileUrl)
+                            print("Temporary file has been deleted")
+                        } catch {
+                            print(error)
+                        }
+                        return
+                    } else {
+                        do {
+                            try FileManager.default.removeItem(at: tempFileUrl)
+                            print("Temporary File has been deleted")
+                        } catch {
+                            print(error)
+                        }
+                    }
+                    if let shareError = error {
+                        print("error while sharing: \(shareError.localizedDescription)")
+                    }
+                }
             }
             
         })
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -126,6 +137,7 @@ class RecordingsViewController: UIViewController, UITableViewDataSource, UITable
             }
         }
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "RecordingCell", for: indexPath) as! RecordingCell
@@ -136,24 +148,14 @@ class RecordingsViewController: UIViewController, UITableViewDataSource, UITable
         cell.nameLabel.text = recording["name"] as? String
 //        cell.dateLabel.text = recording["createdAt"] as? String
         cell.shareButton.tag = indexPath.row
-        cell.shareButton.addTarget(self, action: #selector(whichButtonPressed(sender:)), for: .touchUpInside)
         
         return cell
         // MARK: - Table view data source
     }
     
-    @objc func whichButtonPressed(sender: UIButton) {
-    var buttonNumber = sender.tag
-    }
-    
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return recordings.count
-    }
-
-    func downloadRecording(){
-        
     }
 
     func grabRecording(){
